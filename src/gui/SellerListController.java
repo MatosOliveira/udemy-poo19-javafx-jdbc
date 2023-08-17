0695f64d32a4b0eb99fec6df90b1cfe97fa3e169
MatosOliveira <matosalem.oliveira@gmail.com>
2023-08-17 20:47:27 -0300

    Seller Table view

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -46,6 +47,15 @@ public class SellerListController implements Initializable, DataChangeListener {
 	private TableColumn<Seller, String> tableColumnName;
 
 	@FXML
+	private TableColumn<Seller, String> tableColumnEmail;
+
+	@FXML
+	private TableColumn<Seller, LocalDate> tableColumnBirthDate;
+
+	@FXML
+	private TableColumn<Seller, Double> tableColumnBaseSalary;
+
+	@FXML
 	private TableColumn<Seller, Seller> tableColumnEDIT;
 
 	@FXML
@@ -60,7 +70,7 @@ public class SellerListController implements Initializable, DataChangeListener {
 	public void onBtNewAction(ActionEvent event) {
 		Stage parentStage = Utils.currentStage(event);
 		Seller obj = new Seller();
-		//createDialogForm(obj, "/gui/SellerForm.fxml", parentStage);
+		createDialogForm(obj, "/gui/SellerForm.fxml", parentStage);
 	}
 
 	public void setSellerService(SellerService service) {
@@ -75,6 +85,11 @@ public class SellerListController implements Initializable, DataChangeListener {
 	private void initializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+		tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+		Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
+		tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+		Utils.formatTableColumnDouble(tableColumnBaseSalary, 2);
 
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewSeller.prefHeightProperty().bind(stage.heightProperty());
@@ -88,46 +103,58 @@ public class SellerListController implements Initializable, DataChangeListener {
 		List<Seller> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewSeller.setItems(obsList);
-		//initEditButtons();
+		// initEditButtons();
 		initRemoveButtons();
 	}
 
-	/*
-	 * private void createDialogForm(Seller obj, String absoluteName, Stage
-	 * parentStage) { try { FXMLLoader loader = new
-	 * FXMLLoader(getClass().getResource(absoluteName)); Pane pane = loader.load();
-	 * 
-	 * SellerFormController controller = loader.getController();
-	 * controller.setSeller(obj); controller.setSellerService(new SellerService());
-	 * controller.subscribeDataChangeListener(this); controller.updateFormData();
-	 * 
-	 * Stage dialogStage = new Stage(); dialogStage.setTitle("Enter Seller name");
-	 * dialogStage.setScene(new Scene(pane)); dialogStage.setResizable(false);
-	 * dialogStage.initOwner(parentStage);
-	 * dialogStage.initModality(Modality.WINDOW_MODAL); dialogStage.showAndWait();
-	 * 
-	 * } catch (IOException e) { Alerts.showAlert("IOException",
-	 * "Error loading view", e.getMessage(), AlertType.ERROR); } }
-	 */
+	private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+
+			//SellerFormController controller = loader.getController();
+			//controller.setSeller(obj);
+			//controller.setSellerService(new SellerService());
+			//controller.subscribeDataChangeListener(this);
+			//controller.updateFormData();
+
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Seller name");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+
+		} catch (IOException e) {
+			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+
 	@Override
 	public void onDataChanged() {
 		updateTableView();
 	}
 
-	/*
-	 * private void initEditButtons() {
-	 * 
-	 * tableColumnEDIT.setCellValueFactory(param -> new
-	 * ReadOnlyObjectWrapper<>(param.getValue()));
-	 * 
-	 * tableColumnEDIT.setCellFactory(param -> new TableCell<Seller, Seller>() {
-	 * private final Button button = new Button("edit");
-	 * 
-	 * @Override protected void updateItem(Seller obj, boolean empty) {
-	 * super.updateItem(obj, empty); if (obj == null) { setGraphic(null); return; }
-	 * setGraphic(button); button.setOnAction( event -> createDialogForm(obj,
-	 * "/gui/SellerForm.fxml", Utils.currentStage(event))); } }); }
-	 */
+	private void initEditButtons() {
+
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Seller, Seller>() {
+			private final Button button = new Button("edit");
+
+			@Override
+			protected void updateItem(Seller obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(event -> createDialogForm(obj, "/gui/SellerForm.fxml", Utils.currentStage(event)));
+			}
+		});
+	}
 
 	private void initRemoveButtons() {
 		tableColumnREMOVE.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
@@ -149,15 +176,15 @@ public class SellerListController implements Initializable, DataChangeListener {
 
 	private void removeEntity(Seller obj) {
 		Optional<ButtonType> result = Alerts.showConfirmation("Confirmation", "Are you sure to delete?");
-		
-		if(result.get() == ButtonType.OK) {
-			if(service == null) {
+
+		if (result.get() == ButtonType.OK) {
+			if (service == null) {
 				throw new IllegalStateException("Service was null.");
 			}
 			try {
 				service.remove(obj);
 				updateTableView();
-			} catch(DBIntegrityException e) {
+			} catch (DBIntegrityException e) {
 				Alerts.showAlert("Error removing object", null, e.getMessage(), AlertType.ERROR);
 			}
 		}
